1710973794b2107e007fc925f6854fdc8d25efc5
Laura <keenlaura@hotmail.co.uk>
2021-04-07 11:09:11 +0100

    Changed approach - added credit and debit as arrays so I can print out the history of transactions for the statements

--- lib/account.rb
+++ lib/account.rb
@@ -1,15 +1,20 @@
 class Account
-  attr_reader :balance
+  
+  attr_reader :balance, :credit
 
   def initialize
     @balance = 0
+    @credit = []
+    @debit = []
   end
 
   def deposit(amount)
+    @credit << amount 
     @balance += amount
   end
 
   def withdraw(amount)
+    @debit << amount
     @balance -= amount
   end
 
--- lib/history.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'account.rb'
-
-class History
-
-  def credit
-    10
-  end
-
-end
